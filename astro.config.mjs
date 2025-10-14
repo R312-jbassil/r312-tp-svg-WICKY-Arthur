fefc3644f41c3a980fd22e2907c551c40b5c473e
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2025-10-14 16:05:00 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
+
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@src': '/src',
+      }
+    }
+  },
   adapter: node({
     mode: 'standalone'
   }),
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  i18n: {
+    locales: ["en", "fr"], // Locales you want to support
+    defaultLocale: "en", // Default locale (fallback)
+    routing: {
+      prefixDefaultLocale: false, // Ensures that your default locale is prefixed aswell
+    },
+  },
+
 });
