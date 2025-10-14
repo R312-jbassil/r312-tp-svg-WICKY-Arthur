b5039be537507b44e215b67280716b56fdc12c4e
WICKY-Arthur <arthur.wicky@edu.univ-fcomte.fr>
2025-10-14 15:37:59 +0200

    commit pb.ts

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,5 +1,8 @@
 import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from './pocketbase-types';
-
-const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase;
+var path = '';
+if (import.meta.env.MODE === 'developpement')
+    path = 'http://localhost:8090'
+else path = 'http://localhost:8084'
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
